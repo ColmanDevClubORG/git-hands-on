30b75eb6fcdceff47c44ac59eb7d077800eb4321
Tamir Abutbul <1tamir198@gmail.com>
2024-11-27 22:40:00 +0200

    Add template for objects and arrays

---
+++ index.js
@@ -0,0 +1,10 @@
+window.onload = () => {
+  // TODO create array containing all of the inputs
+
+  // TODO print only the input containing value of "good"
+
+  // TODO print the button when he is clicked, how does this relate to objects?
+
+  //TODO create an object based on all of the input ,
+  // Object key value should look like this { inputID : "input value" }
+};
